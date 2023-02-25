6e092ee95aff07b2a384db6cd8ff904771d05ddd
iOSDevLime <onthelots@naver.com>
2023-02-25 18:09:52 +0900

    w제네릭 타입

--- SwiftUi 정리/1. CustomView/someView.playground/Contents.swift
+++ SwiftUi 정리/1. CustomView/someView.playground/Contents.swift
@@ -7,14 +7,15 @@ import UIKit
 // 따라서, some View에 대해 알아보기 전에 '불투명 반환 타입'이 무엇인지 살펴보자.
 
 
+// MARK: - 제네릭 타입 (Generic Type)
+// some view, 불투명 반환타입을 알아보고 있는데, 왜 때문에 '제네릭' ?
+// 불투명 반환타입은 다른말로 '역 제네릭 타입'이라고 함
+
+
+
+
 // MARK: - 불투명 반환타입(opaque return type)
 
-// 우선 아래와 같은 예시 코드를 살펴보자.
-
-// plusFunc1이란 함수는, 정수형 결과를 반환하도록 구성되어 있다.
-func plusFunc1(count: Int) -> Int {
-    return count + 1
-}
 
 // 여기서 정수형 반환 타입을 바로 구체화된 타입(concrete type)이라고 하며,
 // 하지만, 아래와 같이 작성할 수도 있음
