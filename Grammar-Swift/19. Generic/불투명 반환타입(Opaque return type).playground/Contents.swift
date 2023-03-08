7b93f05c94597426662e2c7c5da9787d4d096215
iOSDevLime <onthelots@naver.com>
2023-03-08 19:35:09 +0900

    불투명 반환타입 학습중..

--- Grammar-Swift/19. Generic/불투명 반환타입(Opaque return type).playground/Contents.swift
+++ Grammar-Swift/19. Generic/불투명 반환타입(Opaque return type).playground/Contents.swift
@@ -22,3 +22,30 @@ isEqual(a: &intA, b: &intB) // 잘 작동하는 동시에, T는 Int란 타입으
 // 정확히 반대로 -> 내부에서는 타입을 알고 있으며, 외부에서는 타입을 알 수 없다는 특징을 가지고 있지 않을까? 역할의 반전!
 // 추가적인 예시코드를 살펴보자면..
 
+// 한 회사에서 RamdomBox란 프로토콜을 만들고, 내부에 Gift란 상품을 Random한 타입으로 설정하고자 함
+protocol RandomBox {
+    func message() -> String
+}
+
+// 생산파트에서는 해당 gift를 RandomBox 프로토콜 규칙에 의해 만들고자 함
+
+// 1. IPadGift는 RandomBox를 준수하게 됩니다.
+struct IPadGift: RandomBox {
+    func message() -> String {
+        return "축하드립니다! IPad에 당첨되셨습니다."
+    }
+}
+
+// 2. PensilGift
+struct PensilGift: RandomBox {
+    func message() -> String {
+        return "최고급 연필입니다!"
+    }
+}
+
+func makeGift() -> RandomBox {
+    return IPadGift()
+}
+
+let gift = makeGift()
+print(gift.message())
