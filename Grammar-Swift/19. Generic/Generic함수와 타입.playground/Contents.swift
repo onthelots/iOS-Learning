3fac9775a1b9050a1d497810990b7b90d6e80987
iOSDevLime <onthelots@naver.com>
2023-03-08 16:31:45 +0900

    Generic 완료

--- Grammar-Swift/19. Generic/Generic함수와 타입.playground/Contents.swift
+++ Grammar-Swift/19. Generic/Generic함수와 타입.playground/Contents.swift
@@ -9,20 +9,40 @@ import UIKit
 
 
 // -- 코드 예시 : 매개변수 a와 b의 값을 전환하는 함수 'swapInts'
-//func swapInts(_ a: inout Int, _ b: inout Int) {
-//
-//    let tempA = a // 섀도우 변수 tempA에 매개변수 a를 할당하고,
-//    a = b // 매개변수 a에는 매개변수 b를 할당하고,
-//    b = tempA // 매개변수 b는 섀도우 변수 a를 할당한다
-//
-//    // 그렇게 되면, a와 b는 값이 서로 Swap 될 것임
-//}
-//var a = 12
-//var b = 20
-//swapInts(&a, &b) // 정상적으로 잘 작동!
+func swap(_ a: inout Int, _ b: inout Int) {
+
+    let tempA = a // 섀도우 변수 tempA에 매개변수 a를 할당하고,
+    a = b // 매개변수 a에는 매개변수 b를 할당하고,
+    b = tempA // 매개변수 b는 섀도우 변수 a를 할당한다
+
+    // 그렇게 되면, a와 b는 값이 서로 Swap 될 것임
+}
+var intA = 12
+var intB = 20
+swap(&intA, &intB) // 정상적으로 잘 작동!
+
+
 
 // 하지만, 정수형 이외 문자 및 Double 타입 또한 동일한 함수를 만들어 주고 싶다면..
 // '오버로딩'을 통해 매개변수의 타입만 다른 함수를 만들어 주어야 했음 -> 귀찮
+
+func swap(_ a: inout String, _ b: inout String) {
+    
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+var stringA = "Lime"
+var stringB = "Jason"
+
+swap(&stringA, &stringB)
+
+stringA // "Jason"
+stringB // "Lime"
+
+
+
 // 이럴때, "어떤 타입이 와도, 활용할 수 있는 함수를 만들 수 없을까?"란 질문을 던질 수 있음
 // 여기서 활용하는 것이 바로 '제네릭' 타입임
 
@@ -56,17 +76,55 @@ string2 // Lime
 // 위에서 살펴본 '함수'로서 활용하는 방법 이외,
 // 구조체, 클래스, 열거형 등 타입에도 선언하여 활용할 수 있음! -> 이를 제네릭 타입이라고 함
 
-struct Stack<T> {
-    let items: [T] = []
+struct Customer<T> {
+    var number: [T] = []
     
-    func push(_ item: T) {
-        print("FIFO에 의하여 추가")
-    }
-    
-    func pop() -> T {
-        print("FIFO에 의하여 삭제 ") as! T
+    mutating func deleteNumber(_ names: T) {
     }
 }
 
+var order: Customer<Int> = .init()
+
+order.number = [1,4,6]
+
+order.number // [1,4,6]
 
 
+// MARK: - 타입 제약(Type Constraints)
+// 앞서 살펴본 제네릭은, 말 그대로 '모든 타입'에서 사용이 가능하다는 특징이 있었음.
+// 하지만, 제네릭을 사용하는 대신 특정 타입만 사용하도록 역으로 조건을 걸 순 없을까?
+// 이를 실현하기 위한 것이 바로 '타입 제약'임
+
+// MARK: - 1. 프로토콜 제약
+// 매개변수 a와 b가 동일한 경우 true, 그렇지 않을 경우 false를 반환하는 함수를 만들어 보자.
+//func isEqual<T>(_ a: T, _ b: T) -> Bool {
+//    return a == b // Binary operator '==' cannot be applied to two 'T' operands 오류 발생
+//}
+
+// 왜? == 란 비교연산자의 경우, a와 b 타입이 'Equtable'이란 프로토콜을 준수할 경우에만 활용이 가능함.
+// 여기서 Equatbale 프로토콜은 쉽게 말해 '값이 동일한지, 어떤지 비교할 수 있는 타입'임 (대부분의 기본 데이터 타입은 해당 프로토콜을 따름)
+
+func isEqual<T: Equatable>(_ a: T, _ b: T) -> Bool {
+    return a == b // Binary operator '==' cannot be applied to two 'T' operands 오류 발생
+}
+
+isEqual(5, 5) // true
+isEqual(10, 1) // false
+
+
+// MARK: - 2. 클래스 제약
+// 프로토콜과 동일하게 활용되나, 해당 키워드에 프로토콜이 아닌 클래스 이름이 오게 됨
+
+class Animals { }
+class Human { }
+class Son: Human { }
+
+func callName<T: Human>(_  a: T) { }
+
+var animals = Animals.init()
+var human = Human.init()
+var son = Son.init()
+
+callName(animals)
+callName(human)
+callName(son) // Global function 'callName' requires that 'Animals' inherit from 'Human'
