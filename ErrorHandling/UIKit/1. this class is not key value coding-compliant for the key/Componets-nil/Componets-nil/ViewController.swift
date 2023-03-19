456a52f81e4757bba28b13f3edbd76b8aab61cbe
iOSDevLime <onthelots@naver.com>
2023-03-19 15:24:21 +0900

    Errorhandling 폴더생성 및 Inspectors Connections 문제 해결

---
+++ ErrorHandling/UIKit/1. this class is not key value coding-compliant for the key/Componets-nil/Componets-nil/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Componets-nil
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // 해당 변수의 이름을 바꾸거나, 삭제(주석)를 할 경우
+    // Connections Inspectors 연결 현황을 직접 관리해야 함!
+    @IBOutlet weak var hello: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ErrorHandling/UIKit/1. this class is not key value coding-compliant for the key/Componets-nil/Componets-nilUITests/Componets_nilUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Componets_nilUITestsLaunchTests.swift
+//  Componets-nilUITests
+//
+//  Created by Jae hyuk Yim on 2023/03/19.
+//
+
+import XCTest
+
+final class Componets_nilUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
