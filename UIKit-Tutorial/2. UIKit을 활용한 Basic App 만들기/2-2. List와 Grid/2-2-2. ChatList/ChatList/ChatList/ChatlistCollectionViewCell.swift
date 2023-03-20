57e680d86072221d11f1c3543af5d775e6b76e89
iOSDevLime <onthelots@naver.com>
2023-03-20 17:46:50 +0900

    Chatlist 파일 생성

---
+++ UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-2. List와 Grid/2-2-2. ChatList/ChatList/ChatList/ChatlistCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChatlistCollectionViewCell.swift
+//  ChatList
+//
+//  Created by Jae hyuk Yim on 2023/03/20.
+//
+
+import UIKit
+
+class ChatlistCollectionViewCell: UICollectionViewCell {
+    
+}
