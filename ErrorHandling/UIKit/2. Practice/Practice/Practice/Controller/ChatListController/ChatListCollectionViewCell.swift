1cf85751ba320e97bf45eb48df24c2297ef0a3c4
iOSDevLime <onthelots@naver.com>
2023-03-21 19:03:42 +0900

    List와 Grid 구현방식 복습

---
+++ ErrorHandling/UIKit/2. Practice/Practice/Practice/Controller/ChatListController/ChatListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChatListCollectionViewCell.swift
+//  Practice
+//
+//  Created by Jae hyuk Yim on 2023/03/21.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ErrorHandling/UIKit/2. Practice/Practice/Practice/Model/ChatListModel/ChatModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatModel.swift
+//  Practice
+//
+//  Created by Jae hyuk Yim on 2023/03/21.
+//
+
+import Foundation
