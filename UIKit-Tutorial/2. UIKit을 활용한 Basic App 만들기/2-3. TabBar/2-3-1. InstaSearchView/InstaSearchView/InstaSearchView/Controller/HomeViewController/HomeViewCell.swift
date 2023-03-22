5f2a6ad7fef76b87ea2bed5fea60d8b3355c17e9
iOSDevLime <onthelots@naver.com>
2023-03-22 16:59:55 +0900

    Tabbar 과제 완료

--- UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-3. TabBar/2-3-1. InstaSearchView/InstaSearchView/InstaSearchView/Controller/HomeViewController/HomeViewCell.swift
+++ UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-3. TabBar/2-3-1. InstaSearchView/InstaSearchView/InstaSearchView/Controller/HomeViewController/HomeViewCell.swift
@@ -11,12 +11,12 @@ class HomeViewCell: UICollectionViewCell {
     
     @IBOutlet weak var thumbnailImage: UIImageView!
     
-    
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        
-//        thumbnailImage.image = nil
-//    }
+    // 재사용 전, 준비해야 할 작업 (이미지 자체를 초기화 한다)
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        thumbnailImage.image = nil
+    }
     // MARK: - Data 할당 메서드
     func configure(_ imageName: String) {
         thumbnailImage.image = UIImage(named: imageName)
