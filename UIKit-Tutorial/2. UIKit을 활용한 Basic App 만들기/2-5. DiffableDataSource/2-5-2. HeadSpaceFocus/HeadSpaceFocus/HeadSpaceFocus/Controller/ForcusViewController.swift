952953f2301c21c4fdcfc9794fddb6b00a0f39b5
iOSDevLime <onthelots@naver.com>
2023-03-24 17:17:09 +0900

    HeadSpaceFocus 프로젝트 완료

--- UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-2. HeadSpaceFocus/HeadSpaceFocus/HeadSpaceFocus/Controller/ForcusViewController.swift
+++ UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-2. HeadSpaceFocus/HeadSpaceFocus/HeadSpaceFocus/Controller/ForcusViewController.swift
@@ -11,6 +11,11 @@ class ForcusViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    // MARK: - Button의 라벨을 바꾸기 위한 flag
+    var curated: Bool = false
+    
     var items: [Focus] = Focus.list
     
     typealias Item = Focus
@@ -44,9 +49,12 @@ class ForcusViewController: UIViewController {
         
         // MARK: - Layout (CompositionalLayout)
         collectionView.collectionViewLayout = layout()
+        
+        refreshButton.layer.shadowOffset = .init(width: 1, height: 1)
+        updateButtonTitle()
     }
     
-    func layout() -> UICollectionViewCompositionalLayout {
+    private func layout() -> UICollectionViewCompositionalLayout {
         
         // MARK: - estimated? (content에 따라서 변경이 될 수 있음) -> 일종의 동적 요소 (AutoLayout에서 잡아주지 않은 이유가 있음)
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
@@ -58,12 +66,37 @@ class ForcusViewController: UIViewController {
         let section = NSCollectionLayoutSection(group: group)
         
         section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
-        
+
         // MARK: - 그룹간의 Spacing
         section.interGroupSpacing = 10
         
         let layout = UICollectionViewCompositionalLayout(section: section)
         
         return layout
+    }
+    
+    // MARK: - Button의 라벨 변경
+    func updateButtonTitle() {
+        let title = curated ? "See All" : "See Recommendation"
+        // setTitle의 인자값으로 위에 선언한 title을 할당
+        refreshButton.setTitle(title, for: .normal)
+    }
+    
+    @IBAction func refreshButtonTapped(_ sender: Any) {
+        
+        curated.toggle() // 토글 Action 추가
+        
+        // items(Focus 구조체에 선언된 배열 데이터들)이 true일땐 recommendation을, 그렇지 않으면 list를
+        self.items = curated ? Focus.recommendations : Focus.list
+        
+        // MARK: - 그럼 업데이트는?? -> 똑같이 snapshot 로직을 돌려줌
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        
+        snapshot.appendSections([.main])
+        // 대신, 여기서 items는 위에서 toggle()값을 할당한 items여야겠지
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        updateButtonTitle()
     }
 }
