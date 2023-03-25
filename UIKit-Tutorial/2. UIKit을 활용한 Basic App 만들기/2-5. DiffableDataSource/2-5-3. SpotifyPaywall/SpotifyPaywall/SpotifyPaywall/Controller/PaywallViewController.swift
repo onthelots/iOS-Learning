a777fea861bc4f8d1258852407133a1982b979ad
iOSDevLime <onthelots@naver.com>
2023-03-25 22:08:30 +0900

    SpotifyPaywall 프로젝트 진행중

--- UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-3. SpotifyPaywall/SpotifyPaywall/SpotifyPaywall/Controller/PaywallViewController.swift
+++ UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-3. SpotifyPaywall/SpotifyPaywall/SpotifyPaywall/Controller/PaywallViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-// https://developer.spotify.com/documentation/general/design-and-branding/#using-our-logo
-// https://developer.apple.com/documentation/uikit/nscollectionlayoutsectionvisibleitemsinvalidationhandler
-// 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
-
 class PaywallViewController: UIViewController {
 
-     override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
+
+
+
+// https://developer.spotify.com/documentation/general/design-and-branding/#using-our-logo
+// https://developer.apple.com/documentation/uikit/nscollectionlayoutsectionvisibleitemsinvalidationhandler
+// 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
