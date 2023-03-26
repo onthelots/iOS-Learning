db56e9f0760881dfdb983c26001bea0e5b7b9949
iOSDevLime <onthelots@naver.com>
2023-03-27 00:28:44 +0900

    Errorhandling 작업중

---
+++ ErrorHandling/UIKit/2. Controlleroutlet cannot be connected to repeating content/CustomCellConnectingError/CustomCellConnectingError/JoshCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  JoshCollectionViewCell.swift
+//  CustomCellConnectingError
+//
+//  Created by Jae hyuk Yim on 2023/03/27.
+//
+
+import UIKit
+
+class JoshCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ErrorHandling/UIKit/2. Controlleroutlet cannot be connected to repeating content/CustomCellConnectingError/CustomCellConnectingError/JoshViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JoshViewController.swift
+//  CustomCellConnectingError
+//
+//  Created by Jae hyuk Yim on 2023/03/27.
+//
+
+import UIKit
+
+class JoshViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
