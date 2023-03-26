d9b03ac7f272c6deec116ce28da6095788b8e580
iOSDevLime <onthelots@naver.com>
2023-03-26 23:52:04 +0900

    SpotifyPaywall 프로젝트 진행중

--- UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-3. SpotifyPaywall/SpotifyPaywall/SpotifyPaywall/Controller/PaywallViewController.swift
+++ UIKit-Tutorial/2. UIKit을 활용한 Basic App 만들기/2-5. DiffableDataSource/2-5-3. SpotifyPaywall/SpotifyPaywall/SpotifyPaywall/Controller/PaywallViewController.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class PaywallViewController: UIViewController {
 
+    let bannerInfo: [BannerInfo] = BannerInfo.list
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     @IBOutlet weak var getTappedButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // pageControl 세팅
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = bannerInfo.count
+    }
+}
+
+// MARK: - pageControl Setting
+extension PaywallViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("STOP")
+        
+        var index = Int(scrollView.contentOffset.x / collectionView.bounds.width)
+        
+        pageControl.numberOfPages = index
     }
 }
 
 
-
-// https://developer.spotify.com/documentation/general/design-and-branding/#using-our-logo
-// https://developer.apple.com/documentation/uikit/nscollectionlayoutsectionvisibleitemsinvalidationhandler
 // 과제: 아래 애플 샘플 코드 다운받아서 돌려보기  https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
