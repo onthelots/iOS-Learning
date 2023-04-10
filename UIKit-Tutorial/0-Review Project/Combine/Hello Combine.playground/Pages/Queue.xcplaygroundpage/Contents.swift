6d81842e5a5474c1e0573624e2baa545bd46f1ad
iOSDevLime <onthelots@naver.com>
2023-04-10 23:24:15 +0900

    Combine-queue 예시코드

---
+++ UIKit-Tutorial/0-Review Project/Combine/Hello Combine.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+import Combine
+import UIKit
+
+DispatchQueue.global().async {
+    // 작업
+}
+
+let queue = DispatchQueue.global()
+queue.async {
+    // 작업
+}
+
+//: [Next](@next)
