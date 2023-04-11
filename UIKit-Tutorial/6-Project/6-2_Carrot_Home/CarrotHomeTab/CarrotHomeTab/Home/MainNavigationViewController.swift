fca3341b9bcc3b0cecebffab925f2d6f6ba29506
iOSDevLime <onthelots@naver.com>
2023-04-11 16:50:35 +0900

    CarrotHomeTab 프로젝트 시작

---
+++ UIKit-Tutorial/6-Project/6-2_Carrot_Home/CarrotHomeTab/CarrotHomeTab/Home/MainNavigationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainNavigationViewController.swift
+//  CarrotHomeTab
+//
+//  Created by Jae hyuk Yim on 2023/04/11.
+//
+
+import UIKit
+
+class MainNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ UIKit-Tutorial/6-Project/6-2_Carrot_Home/CarrotHomeTab/CarrotHomeTab/Home/MainTabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarViewController.swift
+//  CarrotHomeTab
+//
+//  Created by Jae hyuk Yim on 2023/04/11.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
