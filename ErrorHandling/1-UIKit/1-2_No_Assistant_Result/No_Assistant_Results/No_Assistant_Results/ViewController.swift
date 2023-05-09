72d88f03aae2b482f37e326f8f77fbb36ad3a87e
iOSDevLime <onthelots@naver.com>
2023-05-09 10:36:00 +0900

    ErrorHandling 폴더 정리

---
+++ ErrorHandling/1-UIKit/1-2_No_Assistant_Result/No_Assistant_Results/No_Assistant_Results/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  No_Assistant_Results
+//
+//  Created by Jae hyuk Yim on 2023/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
