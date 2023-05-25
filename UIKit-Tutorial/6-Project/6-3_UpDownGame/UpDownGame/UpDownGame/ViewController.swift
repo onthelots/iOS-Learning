2314981378ec757aaadd7f2b124231aed26cbcfa
iOSDevLime <onthelots@naver.com>
2023-05-25 22:38:23 +0900

    UpDown 프로젝트 진행중

--- UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame/UpDownGame/ViewController.swift
+++ UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame/UpDownGame/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        DispatchQueue.main.async {
+            self.mainView.backgroundColor = UIColor.systemGreen
+        }
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
