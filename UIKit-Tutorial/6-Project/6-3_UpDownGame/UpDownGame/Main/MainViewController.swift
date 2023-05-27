535f92d430a250f09375ed13d2fb8abf0534888f
iOSDevLime <onthelots@naver.com>
2023-05-27 16:44:15 +0900

    [Feat] MainView AutoLayout, UI 디자인 1차 완료

--- UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame/Main/MainViewController.swift
+++ UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame/Main/MainViewController.swift
@@ -5,25 +5,33 @@
 //  Created by Jae hyuk Yim on 2023/05/26.
 //
 
+/*
+ [ ] Slider의 현재 위치값 확인, Slider 중앙 상단의 Label값 변동시키기
+ [ ] 체크(V) 버튼을 통해 랜덤값(1~6까지) 생성 및 현재 Slider와의 일치여부 확인
+ [ ] 총 횟수가 5번 이므로, 한번 진행할 때 마다 1씩 증가할 수 있도록 함
+ [ ] Reset은?
+ */
+
 import UIKit
 
 class MainViewController: UIViewController {
     
+    // 상단 - Button 및 Title
     @IBOutlet weak var gameInfoTappedButton: UIButton!
     @IBOutlet weak var gameResetTappedButton: UIButton!
-    @IBOutlet weak var slider: UISlider!
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        slider.value = 3.0
-        slider.minimumValue = 1
-        slider.maximumValue = 6
-        
-    }
+    // 중앙 - Slider 및 Label, Check UIImage
+    @IBOutlet weak var selectedNumber: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    // 하단 - Game 횟수
+    @IBOutlet weak var numOfGame: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setSlider(slider, value: 3.0, minimumValue: 1.0, maximumVAlue: 6.0)
     }
     
     @IBAction func ChangeViewButtonDidTapped(_ sender: Any) {
@@ -36,5 +44,11 @@ class MainViewController: UIViewController {
         
         // Modality
         present(vc, animated: true)
+    }
+    
+    private func setSlider(_ slider: UISlider, value: Float, minimumValue: Float, maximumVAlue: Float) {
+        slider.value = value
+        slider.minimumValue = minimumValue
+        slider.maximumValue = maximumVAlue
     }
 }
