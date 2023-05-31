7f08517f64815db6af2f5d95cad9d7a82a4f1f9f
iOSDevLime <onthelots@naver.com>
2023-05-31 15:20:01 +0900

    Project 완료

--- UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame_v2/UpDownGame/View/GameInfo/GameInfoViewController.swift
+++ UIKit-Tutorial/6-Project/6-3_UpDownGame/UpDownGame_v2/UpDownGame/View/GameInfo/GameInfoViewController.swift
@@ -9,7 +9,22 @@ import UIKit
 
 class GameInfoViewController: UIViewController {
     
+    @IBOutlet weak var gameInfoTextView: UITextView!
+    @IBOutlet weak var dismissButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.gameInfoTextView.text = """
+Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco
+"""
+        
+        self.gameInfoTextView.alignTextViewVerticallyInContainer()
+    }
+    
+    @IBAction func dismissButton(_ sender: Any) {
+        dismiss(animated: true)
     }
 }
