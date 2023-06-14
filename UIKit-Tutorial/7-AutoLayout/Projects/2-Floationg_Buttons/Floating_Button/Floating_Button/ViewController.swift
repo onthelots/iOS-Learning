3ba42a596ad8f541b8d4ef8115cc5ee257dff25e
iOSDevLime <onthelots@naver.com>
2023-06-14 14:56:38 +0900

    Floating Button 프로젝트 완

--- UIKit-Tutorial/7-AutoLayout/Projects/2-Floationg_Buttons/Floating_Button/Floating_Button/ViewController.swift
+++ UIKit-Tutorial/7-AutoLayout/Projects/2-Floationg_Buttons/Floating_Button/Floating_Button/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // menuButton 추가
+        let menuButton = MenuButton()
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(menuButton)
+        
+        menuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                             constant: -20).isActive = true
+        
+        menuButton.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+                                             constant: -20).isActive = true
     }
-
-
 }
 
