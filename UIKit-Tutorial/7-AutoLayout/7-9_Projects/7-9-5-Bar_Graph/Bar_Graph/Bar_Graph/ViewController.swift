2daf753a9f2fe9bf0dc9e330cbe5c02f0e3caf14
iOSDevLime <onthelots@naver.com>
2023-06-19 16:28:50 +0900

    Bar Graph 프로젝트 진행중

--- UIKit-Tutorial/7-AutoLayout/7-9_Projects/7-9-5-Bar_Graph/Bar_Graph/Bar_Graph/ViewController.swift
+++ UIKit-Tutorial/7-AutoLayout/7-9_Projects/7-9-5-Bar_Graph/Bar_Graph/Bar_Graph/ViewController.swift
@@ -32,9 +32,27 @@ class ViewController: UIViewController {
     // randomizeValue -> 해당 bar의 수치인 value의 값을 랜덤으로 설정해줌
     // values 배열의 갯수인 12개를 할당하기 위해, horizontalRange (CloseRange<Int>)를 매핑해서 돌려줌
     private func randomizeValue() {
+//        for _ in horizontalRange {
+//            values = [CGFloat.random(in: 0...100)]
+//        }
         values = horizontalRange.map({ _ in
             CGFloat.random(in: 0...100)
         })
+    }
+    
+    // Constraints
+    private func addConstraint() {
+        barHeightConstraints.forEach { (constraints) in
+            constraints.isActive = false // 가지고 있는 제약이 있다면, 무력화
+            
+            // barHeightConstraints의 값은, multipliers와 barStackView의 SubView를 결합했는데 -> 각각 배열 요소별로 들어오는 heightAnchor는 barSTackView의 높이 X multiplier 값으로 결정됨
+            barHeightConstraints = zip(barHeightMultipliers, barStackView.arrangedSubviews).map {
+                $1.heightAnchor.constraint(equalTo: barStackView.heightAnchor, multiplier: $0)
+            }
+            
+            // 활성화
+            NSLayoutConstraint.activate(barHeightConstraints)
+        }
     }
     
     // drawBars()
@@ -44,6 +62,10 @@ class ViewController: UIViewController {
             bar.backgroundColor = .blue
             barStackView.addArrangedSubview(bar)
         }
+    }
+    
+    @IBAction func randomValueButton() {
+        // barHeightMultiplier의 값을 랜덤하게 돌려주면 될듯
     }
 }
 
