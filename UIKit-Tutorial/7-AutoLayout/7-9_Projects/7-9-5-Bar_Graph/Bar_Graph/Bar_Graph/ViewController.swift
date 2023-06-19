057d092a99c5951f692cd13a7b0991b7e413298b
iOSDevLime <onthelots@naver.com>
2023-06-19 22:44:43 +0900

    Bar Graph 프로젝트 완

--- UIKit-Tutorial/7-AutoLayout/7-9_Projects/7-9-5-Bar_Graph/Bar_Graph/Bar_Graph/ViewController.swift
+++ UIKit-Tutorial/7-AutoLayout/7-9_Projects/7-9-5-Bar_Graph/Bar_Graph/Bar_Graph/ViewController.swift
@@ -21,20 +21,19 @@ class ViewController: UIViewController {
     private var barHeightConstraints: [NSLayoutConstraint] = []
     
     // Multipliers를 통한 bar 높이 결정(배수)
-    private var barHeightMultipliers: [CGFloat] = []
-    
+    private var barHeightMultipliers: [CGFloat] {
+        values.map { $0 / 100}
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        drawBars()
         randomizeValue()
+        drawBars()
+        addConstraint()
     }
     // randomizeValue -> 해당 bar의 수치인 value의 값을 랜덤으로 설정해줌
     // values 배열의 갯수인 12개를 할당하기 위해, horizontalRange (CloseRange<Int>)를 매핑해서 돌려줌
     private func randomizeValue() {
-//        for _ in horizontalRange {
-//            values = [CGFloat.random(in: 0...100)]
-//        }
         values = horizontalRange.map({ _ in
             CGFloat.random(in: 0...100)
         })
@@ -44,15 +43,19 @@ class ViewController: UIViewController {
     private func addConstraint() {
         barHeightConstraints.forEach { (constraints) in
             constraints.isActive = false // 가지고 있는 제약이 있다면, 무력화
-            
-            // barHeightConstraints의 값은, multipliers와 barStackView의 SubView를 결합했는데 -> 각각 배열 요소별로 들어오는 heightAnchor는 barSTackView의 높이 X multiplier 값으로 결정됨
-            barHeightConstraints = zip(barHeightMultipliers, barStackView.arrangedSubviews).map {
-                $1.heightAnchor.constraint(equalTo: barStackView.heightAnchor, multiplier: $0)
-            }
-            
-            // 활성화
-            NSLayoutConstraint.activate(barHeightConstraints)
         }
+        
+        // barHeightConstraints의 값은, multipliers와 barStackView의 SubView를 결합했는데 -> 각각 배열 요소별로 들어오는 heightAnchor는 barSTackView의 높이 X multiplier 값으로 결정됨
+        barHeightConstraints = zip(barStackView.arrangedSubviews, barHeightMultipliers).map({ (eachBar, multiplier) in
+            eachBar.heightAnchor.constraint(equalTo: barStackView.heightAnchor, multiplier: multiplier)
+        })
+        
+        // 활성화
+        NSLayoutConstraint.activate(barHeightConstraints)
+        
+        print(barHeightConstraints.map({ height in
+            height.multiplier
+        }))
     }
     
     // drawBars()
@@ -64,8 +67,13 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func randomValueButton() {
-        // barHeightMultiplier의 값을 랜덤하게 돌려주면 될듯
+    // Reset Values
+    @IBAction func resetValues() {
+        randomizeValue()
+        UIView.animate(withDuration: 0.5) {
+            self.addConstraint()
+            self.view.layoutIfNeeded() // Update Cycle을 바로 실행하기 위함 (해당 메서드가 없을 경우, 애니메이션 효과가 나타나지 않음)
+        }
     }
 }
 
