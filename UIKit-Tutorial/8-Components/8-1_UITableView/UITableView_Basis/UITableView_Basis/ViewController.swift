33f12e0cff97a43cc49dca85dd1bbd2b28a6d9f1
iOSDevLime <onthelots@naver.com>
2023-06-21 20:38:34 +0900

    UITableView Basis 완

--- UIKit-Tutorial/8-Components/8-1_UITableView/UITableView_Basis/UITableView_Basis/ViewController.swift
+++ UIKit-Tutorial/8-Components/8-1_UITableView/UITableView_Basis/UITableView_Basis/ViewController.swift
@@ -16,14 +16,23 @@ class ViewController: UIViewController {
                 ["content3-1", "content3-2", "content3-3", "content3-4"]]
     
     let header = ["Section1", "Section2", "Section3"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .systemGray5
-            self.view.addSubview(self.tableView)
         
+        drawTableView()
+    }
+    
+    // drawTableView()
+    private func drawTableView() {
+        tableView = UITableView()
+        self.view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.automaticallyAdjustsScrollIndicatorInsets = true
+        tableView.dataSource = self
+        tableView.delegate = self
         
         NSLayoutConstraint.activate([
             self.tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -31,8 +40,40 @@ class ViewController: UIViewController {
             self.tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
-
     }
 }
 
+// Data를 할당하는 과정
+extension ViewController: UITableViewDataSource {
+    
+    // row의 수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 미리 작성해 놓은 data의 배열 in 배열요소(section)의 수
+        return data[section].count
+    }
+    
+    // cell 내부에 표현되는 row(item)의 정보
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: .none)
+        
+        // Cell에 표현되는 row(item) -> indexing (미리 작성해놓은 data를 가져오는데, 몇번째 section에 몇번째 row 값인지 설정)
+        cell.textLabel?.text = data[indexPath.section][indexPath.row]
+        return cell
+    }
+}
+
+// Section을 적용하는 과정
+extension ViewController: UITableViewDelegate {
+    
+    // Section의 수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return header.count
+    }
+    
+    // Section의 Title 정의
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return header[section]
+    }
+}
 
