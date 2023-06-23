3f5f24648b69a540a54d48ff7690957d488795db
iOSDevLime <onthelots@naver.com>
2023-06-23 18:03:38 +0900

    iTune API Data Parsing

--- UIKit-Tutorial/4-Network/4-3_Practice(API)/iTunes_API.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ UIKit-Tutorial/4-Network/4-3_Practice(API)/iTunes_API.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -5,19 +5,20 @@ import Combine
 
 
 // MARK: - Model
-struct Apps: Codable {
-    let apps: [AppInfo]
+struct Apps: Codable, Hashable {
+    let apps: [AppInfo] // results
 
     enum CodingKeys: String, CodingKey {
         case apps = "results" // results란 명칭의 키가 존재하기 때문에, 속성 이름은 바꾸지 않거나, CodingKeys를 활용해 맞춰줘야 함
     }
 }
 
-struct AppInfo: Codable {
-    let trackName: String
-    let primaryGenreName: String
-    let description: String
-    let artworkUrl60, artworkUrl512, artworkUrl100: String
+// MARK: - AppInfo
+struct AppInfo: Codable, Hashable {
+    let trackName: String // 앱 이름
+    let primaryGenreName: String // 앱 카테고리
+    let description: String // 앱 소개
+    let artworkUrl60, artworkUrl512, artworkUrl100: String // ImageURL
 }
 
 // MARK: - Error 타입
@@ -77,7 +78,11 @@ let subscription = networkService
     .sink { error in
         print("Error : \(error)")
     } receiveValue: { items in
-        print("Items : \(items)")
+        for apps in items.apps {
+            print("이름 : \(apps.trackName), 소개 : \(apps.description)")
+            print("iconImage : \(apps.artworkUrl60)")
+            print("------------------------------------------")
+        }
     }
 
 
